edd39693f91fb2409b8a74fea673838ccbd5bac2
Panadsada <b6118693@g.sut.ac.th>
2023-04-11 17:23:56 +0700

    edit Line - close#LineNoti

--- src/noti-line/noti-line.controller.ts
+++ src/noti-line/noti-line.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { NotiLineService } from './noti-line.service';
 import { CreateNotiLineDto } from './dto/create-noti-line.dto';
 import { UpdateNotiLineDto } from './dto/update-noti-line.dto';
@@ -8,27 +16,27 @@ export class NotiLineController {
   constructor(private readonly notiLineService: NotiLineService) {}
 
   @Post()
-  async create(@Body() createNotiLineDto: CreateNotiLineDto) {
-    return await this.notiLineService.save(createNotiLineDto);
+  async sendLineNoti() {
+    return await this.notiLineService.sendLineNoti();
   }
 
-  @Get()
-  async findAll() {
-    return await this.notiLineService.findAll();
-  }
+  // @Get()
+  // async findAll() {
+  //   return await this.notiLineService.findAll();
+  // }
 
-  @Get(':id')
-  async findOne(@Param('id') id: string) {
-    return await this.notiLineService.findOne(+id);
-  }
+  // @Get(':id')
+  // async findOne(@Param('id') id: string) {
+  //   return await this.notiLineService.findOne(+id);
+  // }
 
-  @Patch(':id')
-  async update(@Param('id') id: string, @Body() updateNotiLineDto: UpdateNotiLineDto) {
-    return await this.notiLineService.update(+id, updateNotiLineDto);
-  }
+  // @Patch(':id')
+  // async update(@Param('id') id: string, @Body() updateNotiLineDto: UpdateNotiLineDto) {
+  //   return await this.notiLineService.update(+id, updateNotiLineDto);
+  // }
 
-  @Delete(':id')
-  async delete (@Param('id') id: string) {
-    return await this.notiLineService.delete(+id);
-  }
+  // @Delete(':id')
+  // async delete (@Param('id') id: string) {
+  //   return await this.notiLineService.delete(+id);
+  // }
 }
--- src/noti-line/noti-line.module.ts
+++ src/noti-line/noti-line.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { NotiLineService } from './noti-line.service';
 import { HttpModule } from '@nestjs/axios';
+import { UserModule } from 'src/user/user.module';
 import { ScheduleModule } from '@nestjs/schedule';
+import { ScheduleService } from 'src/schedule/schedule.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Schedule } from 'src/schedule/entities/schedule.entity';
+import { Calendar } from 'src/calendar/entities/calendar.entity';
 
 @Module({
-  imports: [HttpModule, ScheduleModule.forRoot()],
+  imports: [
+    HttpModule,
+    TypeOrmModule.forFeature([Schedule, Calendar]),
+    ScheduleModule.forRoot(),
+  ],
   providers: [NotiLineService],
   exports: [NotiLineService],
 })
